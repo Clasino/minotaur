d75f9b334d62ef03c00a514b5a06d3741098c2f9
EPFL <epfl@Cerberus.local>
2017-03-17 16:21:02 +0100

    Functions isRoom, doors, entrance, exit, minotaur implemented

--- Sources/minotaur.swift
+++ Sources/minotaur.swift
@@ -1,66 +1,152 @@
+/*
+ ==============================================================================
+                                Nicolas Cotte
+                                    TP 2
+ ==============================================================================
+*/
+
 import LogicKit
+
+/*
+ ==============================================================================
+                    Enumerations and structures declarations
+ ==============================================================================
+*/
 
 let zero = Value (0)
 
-func succ (_ of: Term) -> Map {
-    return ["succ": of]
-}
-
-func toNat (_ n : Int) -> Term {
-    var result : Term = zero
-    for _ in 1...n {
-        result = succ (result)
-    }
-    return result
-}
-
-struct Position : Equatable, CustomStringConvertible {
+struct Position : Equatable, CustomStringConvertible
+{
     let x : Int
     let y : Int
 
-    var description: String {
+    var description: String
+    {
         return "\(self.x):\(self.y)"
     }
 
-    static func ==(lhs: Position, rhs: Position) -> Bool {
+    static func ==(lhs: Position, rhs: Position) -> Bool
+    {
       return lhs.x == rhs.x && lhs.y == rhs.y
     }
 
 }
 
+/*
+ ==============================================================================
+                            Functions declarations
+ ==============================================================================
+*/
+
+func succ (_ of: Term) -> Map
+{
+    return ["succ": of]
+}
+
+func toNat (_ n : Int) -> Term
+{
+    var result : Term = zero
+    for _ in 1...n
+    {
+        result = succ (result)
+    }
+    return result
+}
 
 // rooms are numbered:
 // x:1,y:1 ... x:n,y:1
 // ...             ...
 // x:1,y:m ... x:n,y:m
-func room (_ x: Int, _ y: Int) -> Term {
-  return Value (Position (x: x, y: y))
+func room (_ x: Int, _ y: Int) -> Term
+{
+    return Value (Position (x: x, y: y))
 }
 
-func doors (from: Term, to: Term) -> Goal {
-    // TODO
+func isRoom (location: Term) -> Goal
+{
+    return (location === room(1, 1)) ||
+           (location === room(1, 2)) ||
+           (location === room(1, 3)) ||
+           (location === room(1, 4)) ||
+           (location === room(2, 1)) ||
+           (location === room(2, 2)) ||
+           (location === room(2, 3)) ||
+           (location === room(2, 4)) ||
+           (location === room(3, 1)) ||
+           (location === room(3, 2)) ||
+           (location === room(3, 3)) ||
+           (location === room(3, 4)) ||
+           (location === room(4, 1)) ||
+           (location === room(4, 2)) ||
+           (location === room(4, 3)) ||
+           (location === room(4, 4))
 }
 
-func entrance (location: Term) -> Goal {
-    // TODO
+func doors (from: Term, to: Term) -> Goal
+{
+    return ((from === room(1, 2)) && (to === room(1, 1))) ||
+           ((from === room(1, 2)) && (to === room(2, 2))) ||
+
+           ((from === room(1, 3)) && (to === room(1, 2))) ||
+
+           ((from === room(1, 4)) && (to === room(1, 3))) ||
+
+
+           ((from === room(2, 1)) && (to === room(1, 1))) ||
+
+           ((from === room(2, 2)) && (to === room(3, 2))) ||
+
+           ((from === room(2, 3)) && (to === room(1, 3))) ||
+           ((from === room(2, 3)) && (to === room(2, 2))) ||
+
+           ((from === room(2, 4)) && (to === room(2, 3))) ||
+
+
+           ((from === room(3, 1)) && (to === room(2, 1))) ||
+
+           ((from === room(3, 2)) && (to === room(3, 3))) ||
+           ((from === room(3, 2)) && (to === room(4, 2))) ||
+
+           ((from === room(3, 4)) && (to === room(2, 4))) ||
+           ((from === room(3, 4)) && (to === room(3, 3))) ||
+
+
+           ((from === room(4, 1)) && (to === room(3, 1))) ||
+
+           ((from === room(4, 2)) && (to === room(4, 1))) ||
+           ((from === room(4, 2)) && (to === room(4, 3))) ||
+
+           ((from === room(4, 4)) && (to === room(3, 4)))
 }
 
-func exit (location: Term) -> Goal {
-    // TODO
+func entrance (location: Term) -> Goal
+{
+    return (location === room(1, 4)) ||
+           (location === room(4, 4))
 }
 
-func minotaur (location: Term) -> Goal {
-    // TODO
+func exit (location: Term) -> Goal
+{
+    return (location === room(1, 1)) ||
+           (location === room(4, 3))
 }
 
-func path (from: Term, to: Term, through: Term) -> Goal {
-    // TODO
+func minotaur (location: Term) -> Goal
+{
+    return location === room(3, 2)
 }
 
-func battery (through: Term, level: Term) -> Goal {
-    // TODO
-}
-
-func winning (through: Term, level: Term) -> Goal {
-    // TODO
-}
+// func path (from: Term, to: Term, through: Term) -> Goal
+// {
+//     // TODO
+// }
+//
+// func battery (through: Term, level: Term) -> Goal
+// {
+//     // TODO
+// }
+//
+// func winning (through: Term, level: Term) -> Goal
+// {
+//     // TODO
+// }
--- Tests/minotaurTests/minotaurTests.swift
+++ Tests/minotaurTests/minotaurTests.swift
@@ -2,97 +2,158 @@ import XCTest
 import LogicKit
 @testable import minotaur
 
-struct Wrapper : Equatable, CustomStringConvertible {
+struct Wrapper : Equatable, CustomStringConvertible
+{
   let term : Term
 
-  var description: String {
+  var description: String
+  {
       return "\(self.term)"
   }
 
-  static func ==(lhs: Wrapper, rhs: Wrapper) -> Bool {
+  static func ==(lhs: Wrapper, rhs: Wrapper) -> Bool
+  {
     return (lhs.term).equals (rhs.term)
   }
 
 }
 
-func resultsOf (goal: Goal, variables: [Variable]) -> [[Variable: Wrapper]] {
+func resultsOf (goal: Goal, variables: [Variable]) -> [[Variable: Wrapper]]
+{
     var result = [[Variable: Wrapper]] ()
-    for s in solve (goal) {
+    for s in solve (goal)
+    {
         let solution  = s.reified ()
         var subresult = [Variable: Wrapper] ()
-        for v in variables {
+        for v in variables
+        {
             subresult [v] = Wrapper (term: solution [v])
         }
-        if !result.contains (where: { x in x == subresult }) {
+        if !result.contains (where: { x in x == subresult })
+        {
             result.append (subresult)
         }
     }
     return result
 }
 
-class minotaurTests: XCTestCase {
+class minotaurTests: XCTestCase
+{
 
-    func testDoors() {
+    func testExample()
+    {
+        print("\n ======================================== Test ======================================== \n")
+
+        let r = Variable(named: "r")
+        let s = Variable(named: "s")
+
+        for solution in solve (isRoom(location: r))
+        {
+            print("* There is a room \(solution[r])")
+        }
+
+        print("")
+
+        for solution in solve (doors(from: r, to: s))
+        {
+            print("* There is a door from \(solution[r]) to \(solution[s])")
+        }
+
+        print("")
+
+        for solution in solve (entrance(location: r))
+        {
+            print("* There is an entrance at \(solution[r])")
+        }
+
+        print("")
+
+        for solution in solve (exit(location: r))
+        {
+            print("* There is an exit at \(solution[r])")
+        }
+
+        print("")
+
+        for solution in solve (minotaur(location: r))
+        {
+            print("* There is a Minotaur at \(solution[r])")
+        }
+
+        print("\n ======================================== Test ======================================== \n")
+    }
+
+    func testDoors()
+    {
         let from = Variable (named: "from")
         let to   = Variable (named: "to")
         let goal = doors (from: from, to: to)
         XCTAssertEqual(resultsOf (goal: goal, variables: [from, to]).count, 18, "number of doors is incorrect")
     }
 
-    func testEntrance() {
+    func testEntrance()
+    {
         let location = Variable (named: "location")
         let goal     = entrance (location: location)
         XCTAssertEqual(resultsOf (goal: goal, variables: [location]).count, 2, "number of entrances is incorrect")
     }
 
-    func testExit() {
+    func testExit()
+    {
         let location = Variable (named: "location")
         let goal     = exit (location: location)
         XCTAssertEqual(resultsOf (goal: goal, variables: [location]).count, 2, "number of exits is incorrect")
     }
 
-    func testMinotaur() {
+    func testMinotaur()
+    {
         let location = Variable (named: "location")
         let goal     = minotaur (location: location)
         XCTAssertEqual(resultsOf (goal: goal, variables: [location]).count, 1, "number of minotaurs is incorrect")
     }
 
-    func testPath() {
-        let through = Variable (named: "through")
-        let goal    = path (from: room (4,4), to: room (3,2), through: through)
-        XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 2, "number of paths is incorrect")
-    }
+    // func testPath()
+    // {
+    //     let through = Variable (named: "through")
+    //     let goal    = path (from: room (4,4), to: room (3,2), through: through)
+    //     XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 2, "number of paths is incorrect")
+    // }
+    //
+    // func testBattery()
+    // {
+    //     let through = Variable (named: "through")
+    //     let goal    = path (from: room (4,4), to: room (3,2), through: through) &&
+    //                   battery (through: through, level: toNat (7))
+    //     XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 1, "number of paths is incorrect")
+    // }
+    //
+    // func testLosing()
+    // {
+    //     let through = Variable (named: "through")
+    //     let goal    = winning (through: through, level: toNat (6))
+    //     XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 0, "number of paths is incorrect")
+    // }
+    //
+    // func testWinning()
+    // {
+    //     let through = Variable (named: "through")
+    //     let goal    = winning (through: through, level: toNat (7))
+    //     XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 1, "number of paths is incorrect")
+    // }
 
-    func testBattery() {
-        let through = Variable (named: "through")
-        let goal    = path (from: room (4,4), to: room (3,2), through: through) &&
-                      battery (through: through, level: toNat (7))
-        XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 1, "number of paths is incorrect")
-    }
 
-    func testLosing() {
-        let through = Variable (named: "through")
-        let goal    = winning (through: through, level: toNat (6))
-        XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 0, "number of paths is incorrect")
-    }
-
-    func testWinning() {
-        let through = Variable (named: "through")
-        let goal    = winning (through: through, level: toNat (7))
-        XCTAssertEqual(resultsOf (goal: goal, variables: [through]).count, 1, "number of paths is incorrect")
-    }
-
-
-    static var allTests : [(String, (minotaurTests) -> () throws -> Void)] {
+    static var allTests : [(String, (minotaurTests) -> () throws -> Void)]
+    {
         return [
+            ("testExample", testExample),
             ("testDoors", testDoors),
             ("testEntrance", testEntrance),
             ("testExit", testExit),
             ("testMinotaur", testMinotaur),
-            ("testPath", testPath),
-            ("testBattery", testBattery),
-            ("testLosing", testLosing),
-            ("testWinning", testWinning),
+            // ("testPath", testPath),
+            // ("testBattery", testBattery),
+            // ("testLosing", testLosing),
+            // ("testWinning", testWinning)
         ]
     }
 }
